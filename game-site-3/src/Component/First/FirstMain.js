d3c78dc665fee259b4cc05bd6b471ffac61b68d3
ehsan <ehssamimi@gmail.com>
2020-01-06 17:17:48 +0330

    change banner

--- game-site-3/src/Component/First/FirstMain.js
+++ game-site-3/src/Component/First/FirstMain.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import Video from './../../Common/background-video2_x264.mp4'
 import button from './../../Common/Asset 2@3x.png';
+import ax from './../../Common/POSTER_FRIEND_new-min.jpg';
 import { Button } from 'reactstrap';
+
 class FirstMain extends Component {
     constructor(props) {
         super(props);
@@ -17,25 +19,28 @@ class FirstMain extends Component {
     }
     render() {
         return (
-            <div className='h-100   '>
-                <video autoPlay muted loop id="myVideo" className='h-100 w-100 fit-fill'>
-                    <source src={Video} type="video/mp4"/>
-                </video>
-                <div className='div-first h-30vh d-flex flex-column' >
-                    <div className='pre text-white fs-35vw'>
-                        <p>FIGHT TO HEAVEN</p>
-                        <p>OR</p>
-                        <p>STAY HELL</p>
-                    </div>
-                    <div className='d-flex flex-column col-10 marginZero paddingZero'>
-                        <button className='button-first w-100  mb-2 fs-17vw   paddingZero'   onClick={this.playVideo.bind(this)}>WATCH TRAILER</button>
-                        <button className='button-first  w-100  mt-2 fs-17vw   paddingZero'>KICK STARTER</button>
-                    </div>
+            <div className='h-100  d-flex align-items-end '>
+                {/*<video autoPlay muted loop id="myVideo" className='h-100 w-100 fit-fill'>*/}
+                    {/*<source src={Video} type="video/mp4"/>*/}
+                {/*</video>*/}
+                <div className='w-100 h-80 '>
+                    <img src={ax} alt="ax" className=" w-100 h-100 fit-cover"/>
                 </div>
-
-                <div className='buttonVideo h-15'>
-                    <img src={button} alt={'button'} className='imgSelf  z-1  pointer svg-b'/>
-                </div>
+                {/*<div className='div-first h-30vh d-flex flex-column' >*/}
+                    {/*<div className='pre text-white fs-35vw'>*/}
+                        {/*<p>FIGHT TO HEAVEN</p>*/}
+                        {/*<p>OR</p>*/}
+                        {/*<p>STAY HELL</p>*/}
+                    {/*</div>*/}
+                    {/*<div className='d-flex flex-column col-10 marginZero paddingZero'>*/}
+                        {/*<button className='button-first w-100  mb-2 fs-17vw   paddingZero'   onClick={this.playVideo.bind(this)}>WATCH TRAILER</button>*/}
+                        {/*<button className='button-first  w-100  mt-2 fs-17vw   paddingZero'>KICK STARTER</button>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
+                
+                {/*<div className='buttonVideo h-15'>*/}
+                    {/*<img src={button} alt={'button'} className='imgSelf  z-1  pointer svg-b'/>*/}
+                {/*</div>*/}
             </div>
         );
     }
